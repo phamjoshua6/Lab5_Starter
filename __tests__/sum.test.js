7e3ebf811b8908151d51116caa94519055675616
phamjoshua6 <phamjoshua6@gmail.com>
2025-05-06 23:50:51 -0700

    failTest

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,54 @@
 // sum.test.js
 
-test('adds 1 + 2 to equal 3', () => {
-  // TODO
+
+import functions from '../code-to-unit-test/unit-test-me.js';
+
+neNumber
+test('isPhoneNumber - valid cases', () => {
+  expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+  expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('isPhoneNumber - invalid cases', () => {
+  expect(functions.isPhoneNumber('123456789')).toBe(false);
+  expect(functions.isPhoneNumber('abc-def-ghij')).toBe(false);
+});
+
+// isEmail
+test('isEmail - valid cases', () => {
+  expect(functions.isEmail('test@example.com')).toBe(true);
+  expect(functions.isEmail('user.name@domain.co')).toBe(true);
+});
+test('isEmail - invalid cases', () => {
+  expect(functions.isEmail('test@.com')).toBe(false);
+  expect(functions.isEmail('user@domain')).toBe(false);
+});
+
+// isStrongPassword
+test('isStrongPassword - valid cases', () => {
+  expect(functions.isStrongPassword('Abc12345')).toBe(true);
+  expect(functions.isStrongPassword('Zyx987_!')).toBe(true);
+});
+test('isStrongPassword - invalid cases', () => {
+  expect(functions.isStrongPassword('123')).toBe(false);
+  expect(functions.isStrongPassword('abc')).toBe(false);
+});
+
+// isDate
+test('isDate - valid cases', () => {
+  expect(functions.isDate('12/25/2020')).toBe(true);
+  expect(functions.isDate('01/01/2000')).toBe(true);
+});
+test('isDate - invalid cases', () => {
+  expect(functions.isDate('2020/12/25')).toBe(false);
+  expect(functions.isDate('13/01/2000')).toBe(false);
+});
+
+// isHexColor
+test('isHexColor - valid cases', () => {
+  expect(functions.isHexColor('#FFF')).toBe(true);
+  expect(functions.isHexColor('#123ABC')).toBe(true);
+});
+test('isHexColor - invalid cases', () => {
+  expect(functions.isHexColor('123456')).toBe(false);
+  expect(functions.isHexColor('#ZZZZZZ')).toBe(false);
 });
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,13 +1,51 @@
-// unit.test.js
-const functions = require('../code-to-unit-test/unit-test-me.js');
-
 import {
   isPhoneNumber,
   isEmail,
   isStrongPassword,
   isDate,
-  isHexColor,
-  functions
-} from '../code-to-unit-test/unit-test-me';
+  isHexColor
+} from '../code-to-unit-test/unit-test-me.js';
 
-// TODO - Part 2
+test('isPhoneNumber - valid cases', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('isPhoneNumber - invalid cases', () => {
+  expect(isPhoneNumber('123456789')).toBe(false);
+  expect(isPhoneNumber('abc-def-ghij')).toBe(false);
+});
+
+test('isEmail - valid cases', () => {
+  expect(isEmail('test@example.com')).toBe(false);   expect(isEmail('username@domain.co')).toBe(true); 
+});
+test('isEmail - invalid cases', () => {
+  expect(isEmail('test@.com')).toBe(false);
+  expect(isEmail('user@domain')).toBe(false);
+});
+
+test('isStrongPassword - valid cases', () => {
+  expect(isStrongPassword('Abc12345')).toBe(true);
+  expect(isStrongPassword('Zyx987_')).toBe(true); 
+});
+test('isStrongPassword - invalid cases', () => {
+  expect(isStrongPassword('123')).toBe(false);
+  expect(isStrongPassword('abc')).toBe(false);
+});
+
+test('isDate - valid cases', () => {
+  expect(isDate('12/25/2020')).toBe(true);
+  expect(isDate('01/01/2000')).toBe(true);
+});
+test('isDate - invalid cases', () => {
+  expect(isDate('2020/12/25')).toBe(false);
+  expect(isDate('abc')).toBe(false); 
+});
+
+test('isHexColor - valid cases', () => {
+  expect(isHexColor('#FFF')).toBe(true);
+  expect(isHexColor('#123ABC')).toBe(true);
+});
+test('isHexColor - invalid cases', () => {
+  expect(isHexColor('12345')).toBe(false);
+  expect(isHexColor('#ZZZZZZ')).toBe(false);
+});
